733a2dd3817406e1f28f9700f82b1d085eeafd3a
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-10-26 18:21:54 +0530

    MNIST etc packed

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,26 @@
 module load python/3.7.0
 module load pytorch/1.0.0
 
+FILES=(
+    cifar-10.tar
+    mnist.tar
+    svhn.tar
+)
+
+TORCHVISION_PATH="ada:/share1/dataset/torchvision"
+WORK_DIR="/ssd_scratch/cvit/$USER/dpn"
+mkdir -p $WORK_DIR
+
+set -x
+
+function copy {
+    for FILE in ${FILES[@]}; do
+        rsync $TORCHVISION_PATH/$FILE $WORK_DIR/
+        cd $WORK_DIR && tar -xvf $FILE && cd -;
+    done;
+}
+
+
 python3 -m dpn.main \
     --alpha 1e3 \
     --epochs 1000 \
@@ -10,6 +30,6 @@ python3 -m dpn.main \
     --batch_size 256 \
     --momentum 0.9  \
     --lr 1e-3       \
-    --work_dir /ssd_scratch/cvit/$USER/ \
+    --work_dir $WORK_DIR \
     --weight_decay 0.0 \
     --model vgg6
