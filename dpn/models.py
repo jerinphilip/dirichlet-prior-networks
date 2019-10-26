a5d01c7ef2fdd31b2ee4f77fa4bc2246c58444a4
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-10-26 18:21:29 +0530

    Adding MLP to reproduce plots

--- dpn/models.py
+++ dpn/models.py
@@ -124,3 +124,20 @@ class SimpleConvModel(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+class VanillaMLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        self.network = nn.Sequential(
+                nn.Linear(input_size, hidden_size),
+                nn.Sigmoid()
+                nn.Linear(hidden_size, output_size)
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
