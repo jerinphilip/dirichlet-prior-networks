d2622b171f56e2f63656a18a148d9767483223bc
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-08-09 03:48:14 +0530

    Cleaning out dpn; Removing

--- scripts/synthetic_data.sh
+++ scripts/synthetic_data.sh
@@ -8,19 +8,20 @@ mkdir -p $WORK_DIR
 
 function f {
     python3 -m dpn.exps.synthetic_data \
-        --alpha 1e3 --epochs 4 --device cuda \
-        --batch_size 128 --momentum 0.9 --lr 1e-3 \
-        --weight_decay 0.05 --work_dir $WORK_DIR \
-        --model mlp --dataset synthetic --radius 4.0 \
-        --sigma 1.0 --shuffle \
+        --alpha 1e3 --epochs 10 --device cuda \
+        --batch_size 1024 --momentum 0.9 --lr 1e-3 \
+        --weight_decay 0.0 --work_dir $WORK_DIR \
+        --model mlp --dataset synthetic --radius 8.0 \
+        --sigma 2.5 --shuffle \
         --num_train_samples 100000 --num_test_samples 1000 \
         --log --ind-loss "$1" --ood-loss "$2"\
         --output-dir $WORK_DIR  \
         --num_points 200 --log_interval 10 \
-        --rejection-threshold 1.0672106745950928e-05 # norm.pdf(3.1)**2
+        --rejection-threshold 1e-5
+        # --rejection-threshold 1.0672106745950928e-05 # norm.pdf(3.1)**2
 }
 
 # f "{'cross_entropy': 1.0}"
 f "{'dirichlet_kldiv': 1.0, 'cross_entropy': 1.0}" "{'dirichlet_kldiv': 1.0}"
-# f "{'dirichlet_kldiv': 1.0}"
+# f "{'dirichlet_kldiv': 1.0}" "{'dirichlet_kldiv': 1.0}"
 # f "{'nll': 1.0, 'mutual_information': 1.0}"
